87360cf337c425bec967d2a37c81681a2247ad27
Devon M <devon.l.mckee@gmail.com>
2018-06-29 17:35:20 -0700

    fetch items in /MSWeb/data/ ending in .txt for select

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -4,11 +4,16 @@ import enable_cgitb
 import os
 
 def main():
-    files = os.listdir(os.path.join(os.pardir, 'data'))
+    files = []
+    for file in os.listdir(os.path.join(os.pardir, 'data')):
+        if file.endswith(".txt"):
+            files.append(file)
     print("Content-Type: text/html")
     print("")
-    for file in files:
-        print("<option>"+str(file)+"</option>")
-
+    if not files:
+        print("<option>No files in data directory!</option>")
+    else:
+        for file in files:
+            print("<option>"+str(file)+"</option>")
 if __name__ == "__main__":
     main()
--- html/scripts/ajaxtest.js
+++ html/scripts/ajaxtest.js
@@ -2,7 +2,14 @@ function loadList() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("datalist").innerHTMl = this.responseText;
+            select = document.getElementById("datalist");
+            text = this.responseText;
+            items = text.split("\n");
+            for(var i=0; i<(items.length-1);i++){
+                var opt = document.createElement("option");
+                opt.innerHTML = items[i];
+                select.appendChild(opt);
+            }
         }
     };
     xhttp.open("GET", "/MSWeb/cgi-bin/index.py", true);
