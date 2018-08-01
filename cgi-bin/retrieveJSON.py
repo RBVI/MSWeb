5a7eee2a533b9b16cd33b2357b4a747efd5792c5
Devon M <devon.l.mckee@gmail.com>
2018-08-01 10:54:25 -0700

    JSON hashing / changing JSON structure / retrieveJSON.py
    - adds "hash" to metadata of JSON file, simple sha256 hash to represent data file in server, all hashes stored (with respective filenames) in hashes.json.
    - changing JSON structure from being row-wise (better for grabbing certain data points) to column-wise (better for grabbing data from specific columns)
    - adds retrieveJSON.py, python file (not implemented yet, for next commit) to retrieve JSON data from server when given a hash value via get request header and then sends JSON data back to server.

---
+++ cgi-bin/retrieveJSON.py
@@ -0,0 +1,18 @@
+import json, os, cgi, enable_cgitb
+
+def main():
+    form = cgi.FieldStorage()
+    args = {}
+    for arg in form:
+        args[arg] = form.getvalue(arg)
+    with open(os.path.join(os.pardir, "data", "hashes.json")) as fp:
+        hashes = json.load(fp)
+    jsonName = hashes[arg["hash"]]
+    with open(os.path.join(os.pardir, "data", "parsed-data", jsonName)) as fp:
+        jsonFile = json.load(fp)
+    print("Content-Type: application/json")
+    print("")
+    print(json.dumps(jsonFile))
+    
+if __name__ == "__main__":
+    main()
