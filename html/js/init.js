11ef6668059fdd524d608429d0ea04976c1cc19e
Devon M <devon.l.mckee@gmail.com>
2018-08-01 14:42:22 -0700

    initial column selectors

--- html/js/init.js
+++ html/js/init.js
@@ -1,6 +1,11 @@
 // put JS code that needs to connect two layout elements together in here
 // declare layout-transcending functions here
-
+function removeOptions(selectbox) { // removes <option> tags from <select> tags 
+    var i;
+    for(i = selectbox.options.length - 1 ; i >= 0 ; i--) {
+        selectbox.remove(i);
+    }
+}
 // only call declared functions here, DO NOT declare functions in init() otherwise it will get really messy
 function init() {
     $('body').layout({
