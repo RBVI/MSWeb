73ff83990145f3dcedf2f14a3e741222e79e78f7
AB <yahskagb@gmail.com>
2018-08-02 10:52:01 -0700

    Changed position of JSON code

--- html/js/center.js
+++ html/js/center.js
@@ -2,8 +2,8 @@ var center = function() {
     // put code in init() if you need it to run at runtime
     // call previously declared functions, do not declare functions in init()
     function init() {
-        console.log("center.js loaded and initialized");
         $('#my-select').multiSelect();
+        console.log("center.js loaded and initialized");
     }
     //declare functions here
     return {
