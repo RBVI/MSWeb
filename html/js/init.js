0e2da4fea9f7d627d3fa27ec74ce896396f44832
Devon M <devon.l.mckee@gmail.com>
2018-08-06 11:05:39 -0700

    demo page layout update 3

--- html/js/init.js
+++ html/js/init.js
@@ -1,11 +1,18 @@
 // put JS code that needs to connect two layout elements together in here
 // declare layout-transcending functions here
-
+window.onresize = function() {
+    var plot1 = document.getElementById("plot1");
+    Plotly.Plots.resize(plot1);
+}
 // only call declared functions here, DO NOT declare functions in init() otherwise it will get really messy
 function init() {
     $('body').layout({
         south__size: "60%",
-        west__size: "25%"
+        west__size: "25%",
+        west__onresize: function() {
+            var plot1 = document.getElementById("plot1");
+            Plotly.Plots.resize(plot1);
+        }
     });
     west.init();
     center.init();
