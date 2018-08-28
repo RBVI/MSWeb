6a15bca00b2e84a184abaa062db5b9f8f2f367cd
Devon M <devon.l.mckee@gmail.com>
2018-08-28 15:39:10 -0700

    clearing branch for standalone test

--- README.md
+++
@@ -1,7 +0,0 @@
-# MSWeb
-hello
-hello2
-hello3
-hello4
-hello5
-hello6
--- cgi-bin/enable_cgitb.py
+++
@@ -1,5 +0,0 @@
-import cgitb
-# Fix stupid Python cgitb extraneous output
-cgitb._reset = cgitb.reset
-cgitb.reset = lambda: cgitb._reset().replace('<!--: spam\n', '')
-cgitb.enable()
